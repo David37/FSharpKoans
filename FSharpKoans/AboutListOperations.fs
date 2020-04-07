c5b22e59eaba48f99acb1f530cbc056907de983d
David37 <davidkondic@protonmail.com>
2020-04-07 19:37:54 +0200

    Completed AboutLists and AboutStrings

--- FSharpKoans/AboutListOperations.fs
+++ FSharpKoans/AboutListOperations.fs
@@ -29,7 +29,11 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``01 Finding the length of a list, the hard way`` () =
         let length (xs : 'a list) : int =
-            __ // write a function to find the length of a list
+            let rec findLength list count=
+                match list with
+                | [] -> count
+                | head::tail -> findLength tail count+1
+            findLength xs 0 // write a function to find the length of a list
         length [9;8;7] |> should equal 3
         length [] |> should equal 0
         length ["Le Comte de Monte-Cristo"] |> should equal 1
@@ -38,7 +42,7 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     // Hint: https://msdn.microsoft.com/en-us/library/ee340354.aspx
     [<Test>]
     let ``02 Finding the length of a list, the easy way`` () =
-        __ [9;8;5;8;45] |> should equal 5
+        List.length [9;8;5;8;45] |> should equal 5
 
     [<Test>]
     let ``03 Reversing a list, the hard way`` () =
